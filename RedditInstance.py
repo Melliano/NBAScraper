051cc8426c318519f2bf334c303523f6a2baa971
Melliano <callummelia@gmail.com>
2019-02-18 20:18:26 +0000

    second commit did this work i hate vim#

--- RedditInstance.py
+++ RedditInstance.py
@@ -1,4 +1,5 @@
 import praw
+import datetime
 
 
 class Reddit:
@@ -9,22 +10,46 @@ class Reddit:
                                   user_agent='Melliano user agent here')   
         self.subreddit = self.reddit.subreddit('nba')
         self.gameThread = 'game thread'
+        self.submissionsAlreadyRead = []
 
     def getGameThreads(self, subreddit, gameThread):
         # Get submissions objects of previous game thread
         # Limited to 100
-        submissions = []
+        submissionsToBeRead = []
         for submission in subreddit.search(gameThread):
-            submissions.append(submission)
-        return submissions
+            if submission not in self.submissionsAlreadyRead:
+                submissionsToBeRead.append(submission)
+        return submissionsToBeRead
 
     def getCommentsFromGameThread(self, submission):
+        # Allows for the comment limit of more comments
+        # To be removed and returna flat response of all comments
+        submission.comments.replace_more(limit=0)
         comments = submission.comments.list()
+        counter = 0        
+        for comment in comments:
+            print(str(counter) + " -  " + comment.body + "\n")
+            counter += 1
+        self.submissionsAlreadyRead.append(submission)    
+        self.saveGameThreadToFile(submission, comments)
         return comments
 
+    def getCommentsForAllSubmissions(self, submissions):
+        for submission in submissions:
+            print(submission.title + "\n")
+            self.getCommentsFromGameThread(submission)            
+
+    def saveGameThreadToFile(self, submission, comments):
+        file = open("test.txt", "w+")
+        file.write(comments)
+        file.close()        
 
 if __name__ == "__main__":
+    startTime = datetime.datetime.now()
     redditInstance = Reddit()
     submissions = redditInstance.getGameThreads(redditInstance.subreddit,                                    
                                                 redditInstance.gameThread)                          
-    print("Number of submissions - " + str(len(submissions)))
+    print("Number of submissions - " + str(len(submissions)))    
+    allComments = redditInstance.getCommentsForAllSubmissions(submissions)    
+    endTime = datetime.datetime.now() - startTime    
+    print("Finished in " + str(endTime))
